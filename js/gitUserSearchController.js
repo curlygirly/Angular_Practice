e46b185471abd4f10896151ee045e39bbaa3009d
Jennifer Arad-Marks <jlaradmarks@gmail.com>
2015-06-09 16:23:37 +0100

    All tests passing

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,27 +1,6 @@
 githubUserSearch.controller('GitUserSearchController', function($controller){
 
 var self = this;
-  self.searchResult = {
-    "items":[
-      {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    },
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    },
-     {
-      "login": "curlygirly",
-      "avatar_url": "https://avatars1.githubusercontent.com/u/11654909?v=3",
-      "html_url": "https://github.com/curlygirly"
-      }
-    ]
-  };
-
-
   self.doSearch = function(){
       self.searchResult = {
         items:[
