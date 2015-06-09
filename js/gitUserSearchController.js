c91e451a3421c53fbffa1fe7467edfa488182679
Jennifer Arad-Marks <jlaradmarks@gmail.com>
2015-06-09 16:49:02 +0100

    Data Binding, pt 2 and Github API - all tests passing

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,25 +1,11 @@
-githubUserSearch.controller('GitUserSearchController', function($controller){
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource){
+
+var searchResource = $resource('https://api.github.com/search/users');
 
 var self = this;
   self.doSearch = function(){
-      self.searchResult = {
-        items:[
-         {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      },
-       {
-        "login": "curlygirly",
-        "avatar_url": "https://avatars1.githubusercontent.com/u/11654909?v=3",
-        "html_url": "https://github.com/curlygirly"
-        }
-      ]
+      self.searchResult = searchResource.get(
+        { q: self.searchTerm }
+      );
     };
-  };
-});
+}]);
