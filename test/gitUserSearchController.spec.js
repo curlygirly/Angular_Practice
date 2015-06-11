404108177f104db171e1c89f70e427d47516ddf5
Spike <spike01@gmail.com>
2015-06-11 11:44:41 +0100

    Update gitUserSearchController.spec.js

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,41 +1,32 @@
 describe('GitUserSearchController', function(){
   beforeEach(module('GitUserSearch'));
 
-  var ctrl;
-  lconsole.log('You are begining to annoy me!!')
-  var items = helperMethod();
+  var ctrl, scope, fakeService, q;
 
-
-  beforeEach(inject(function($controller){
-    ctrl = $controller('GitUserSearchController');
+  beforeEach(inject(function($controller, $rootScope, $q){
+    scope = $rootScope.$new();
+    fakeSearch = jasmine.createSpyObj('fakeSearch', ['query']); // here we create and inject a fakeService with a 'query' property
+    q = $q;
+    ctrl = $controller('GitUserSearchController', { Search: fakeSearch });
   }));
+
 
   it('initialises with an empty search result and term', function(){
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
-
-    });
+  });
 
   describe('when searching for a user', function(){
-       var httpBackend;
-    beforeEach(inject(function($httpBackend){
-    httpBackend = $httpBackend
-    httpBackend
-        .expectGET("https://api.github.com/search/users?q=hello")
-        .respond(
-          { items: items }
-          );
-    }));
 
-    afterEach(function(){
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
+    beforeEach(function() {
+      fakeSearch.query.and.returnValue(q.when({ data: { items: "hello" }})); // returns a "fake" promise
     });
 
     it('displays search results', function(){
+      var items = "hello";
       ctrl.searchTerm = 'hello';
       ctrl.doSearch();
-      httpBackend.flush();
+      scope.$apply(); // this resolves the promise
       expect(ctrl.searchResult.items).toEqual(items);
     });
   });
